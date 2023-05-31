53205ccaed8d6e66f031fa28825166b4f812a469
jahnvi joshi <jahnvijoshi312@gmail.com>
2023-05-31 20:15:47 +0530

    changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+if (profile.name === "dev") {
+  mongoose.connect("mongodb://localhost:27017/issues");
 
-mongoose.connect('mongodb://localhost:27017/issues');
+  const db = mongoose.connection;
 
-const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+  db.once("open", function () {
+    console.log("Connected to Database :: MongoDB");
+  });
 
-
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
-});
-
-
-module.exports = db;
+  module.exports = db;
+}
